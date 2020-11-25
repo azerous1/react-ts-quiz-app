9e6174d11fcc09064e70df8e540184f22231bca5
Yichi Zhang <arthur233333@gmail.com>
2020-11-24 23:11:54 -0800

    change == to ===

--- src/utils/generateComment.ts
+++ src/utils/generateComment.ts
@@ -1,10 +1,10 @@
 const generateComment = (percentOfCorrectAnswer: number): string => {
-    return percentOfCorrectAnswer == 1 ? "You got them all! Great Job!"
+    return percentOfCorrectAnswer === 1 ? "You got them all! Great Job!"
         : percentOfCorrectAnswer > 0.7 ? "Almost there! Keep up the good work!"
             : percentOfCorrectAnswer > 0.5 ? "You got more than half of them correct!"
                 : percentOfCorrectAnswer > 0.3 ? "Good try Good try!"
                     :percentOfCorrectAnswer > 0 ? "That was tough, try it again!"
-                    : percentOfCorrectAnswer == 0 ? "Oops, you miss all the questions. Try it again!" :
+                    : percentOfCorrectAnswer === 0 ? "Oops, you miss all the questions. Try it again!" :
                             "error"
 };
 
